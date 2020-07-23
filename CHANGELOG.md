540839a531162959f6069daa61464bd6cfe1d856
Nitish Raj Uprety <uprety.nitish@gmail.com>
2020-07-23 10:50:42 +0545

    Converted the widget to StateFulWidget.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1] - 22/07/2020.
+
+* Converted the widget to StateFulWidget.
+
 ## [1.0.0] - 22/07/2020.
 
 * First release.
--- lib/circle_button.dart
+++ lib/circle_button.dart
@@ -2,7 +2,7 @@ library circle_button;
 
 import 'package:flutter/material.dart';
 
-class CircleButton extends StatelessWidget {
+class CircleButton extends StatefulWidget {
 
   final VoidCallback onTap;
   final Widget child;
@@ -18,25 +18,30 @@ class CircleButton extends StatelessWidget {
   const CircleButton({Key key, @required this.onTap, this.child, this.width, this.height, this.borderColor, this.borderWidth, this.borderStyle, this.tooltip, this.backgroundColor,}) : super(key: key);
 
   @override
+  _CircleButtonState createState() => _CircleButtonState();
+}
+
+class _CircleButtonState extends State<CircleButton> {
+  @override
   Widget build(BuildContext context) {
     return Tooltip(
-      message: tooltip ?? '',
+      message: widget.tooltip ?? '',
       child: GestureDetector(
-        onTap: onTap,
+        onTap: widget.onTap,
         child: Container(
-          width: width ?? 40,
-          height: height ?? 40,
+          width: widget.width ?? 40,
+          height: widget.height ?? 40,
           decoration: BoxDecoration(
-            color: backgroundColor ?? Colors.transparent,
+            color: widget.backgroundColor ?? Colors.transparent,
             shape: BoxShape.circle,
             border: Border.all(
-              color: borderColor ?? Theme.of(context).primaryColor,
-              style: borderStyle ?? BorderStyle.solid,
-              width: borderWidth ?? 1,
+              color: widget.borderColor ?? Theme.of(context).primaryColor,
+              style: widget.borderStyle ?? BorderStyle.solid,
+              width: widget.borderWidth ?? 1,
             ),
           ),
           child: Center(
-            child: child,
+            child: widget.child,
           ),
         ),
       ),
